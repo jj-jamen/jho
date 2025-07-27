34138b80482233ce59249c64bd670d4e1331f9b2
unknown <hello.jjjamen@gmail.com>
2025-07-28 01:04:10 +0800

    deploying to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import DesktopView from "./DesktopView";
+import ViewOnDesktop from "./ViewOnDesktop";
 import React from "react";
 
 function App() {
   return (
     <>
       <DesktopView />
+      <ViewOnDesktop />
     </>
   );
 }
---
+++ src/ViewOnDesktop.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ViewOnDesktopRoute() {
+  return (
+    <div className="flex flex-col 2xl:hidden items-center justify-center min-h-screen">
+      <p className="text-lg text-gray-600">This page is best viewed on a desktop device.</p>
+    </div>
+  );
+};
