b3553827e016c1ece087366a9ade9544cce26e02
unknown <hello.jjjamen@gmail.com>
2025-07-28 20:49:21 +0800

    employment section done

--- src/ViewOnDesktop.jsx
+++ src/ViewOnDesktop.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ViewOnDesktopRoute() {
   return (
     <div className="flex flex-col 2xl:hidden items-center justify-center min-h-screen">
-      <p className="text-lg text-gray-600">This page is best viewed on a desktop device.</p>
+      <p className="text-lg text-gray-600">This page is best viewed on a full width desktop browser.</p>
     </div>
   );
 };
